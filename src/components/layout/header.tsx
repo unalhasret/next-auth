a29ac7202b28c7c0a826164842aa4f32b01561b8
Hasret Sultan Ünal <s.hasretunal@gmail.com>
2025-06-30 16:47:17 +0300

    feat: add header component

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return(
+        <header className="bg-black flex h-20 w-full items-center justify-between text-3xl px-6 mb-6">
+            <span className="text-slate-200 font-bold ">Next-Auth</span>
+        </header>
+    )
+}
