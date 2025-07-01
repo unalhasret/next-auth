8e1b0056461c5ba81863ca96809383cf62c282e8
Hasret Sultan Ünal <s.hasretunal@gmail.com>
2025-07-01 21:24:49 +0300

    style: change button width

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -7,7 +7,7 @@ export default function Login() {
       <button
         onClick={() => signIn()}
         type="button"
-        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
+        className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 w-28"
       >
         Sign in
       </button>
